741ab522d9798f21184dc0e06254af8ae0f3c818
saraa93-bit <saraa93177228@gmail.com>
2024-08-12 23:17:05 +0300

    interview

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations needed to result in exactly
+    n H characters in the file."""
+    if n <= 1:
+        return 0
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(int(n / i)) + i
