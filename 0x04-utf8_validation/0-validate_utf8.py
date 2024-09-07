d168fdbce8bcc746e0227d16d8fe87ef551b0e22
saraa93-bit <saraa93177228@gmail.com>
2024-09-07 16:33:23 +0300

    utf8-validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,7 @@
 Module to validate UTF-8 encoding
 """
 
+
 def validUTF8(data):
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
@@ -17,19 +18,25 @@ def validUTF8(data):
         byte = num & 0xFF
 
         if num_bytes == 0:
-            # Determine how many bytes are in the current UTF-8 character
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
+            # Determine how many bytes
+            if (byte & mask1) == 0:
+                # 1-byte character (ASCII)
+                continue
+            elif (byte & (mask1 >> 1)) == 0:
+                return False
+            while (byte & mask1):
+                num_bytes += 1
+                mask1 >>= 1
+
+            # The first byte of a UTF-8 character
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
             # Check that the byte starts with '10'
-            if (byte >> 6) != 0b10:
+            if not (byte & mask1 and not (byte & mask2)):
                 return False
+
+        # Decrement the number of bytes remaining
         num_bytes -= 1
 
     # If there are leftover bytes, return False
