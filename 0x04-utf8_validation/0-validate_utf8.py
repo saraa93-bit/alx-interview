0e11aac51e46e56afe0d7316f5450ff2f6272d94
saraa93-bit <saraa93177228@gmail.com>
2024-09-07 16:24:06 +0300

    utf8-validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,42 @@
+#!/usr/bin/python3
+"""
+Module to validate UTF-8 encoding
+"""
+
 def validUTF8(data):
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    # Masks to check the leading bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    # Masks to identify the leading bits of a byte
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
+    # Loop through each integer in the data list
     for num in data:
-        # Get the 8 least significant bits
+        # Mask to get only the 8 least significant bits
         byte = num & 0xFF
 
-        # If num_bytes is 0, then this is a new character
         if num_bytes == 0:
-            # Determine the number of bytes in the character
+            # Determine how many bytes are in the current UTF-8 character
             if (byte & mask1) == 0:
-                continue  # 1-byte character
+                # 1-byte character (ASCII)
+                continue
             elif (byte & (mask1 >> 1)) == 0:
-                num_bytes = 1  # 2-byte character
-            elif (byte & (mask1 >> 2)) == 0:
-                num_bytes = 2  # 3-byte character
-            elif (byte & (mask1 >> 3)) == 0:
-                num_bytes = 3  # 4-byte character
-            else:
-                return False  # Invalid UTF-8 character
-        else:
-            # If num_bytes > 0, check if the current byte is a continuation byte (starts with '10')
-            if (byte & mask1) == 0 or (byte & mask2) != 0:
                 return False
-            num_bytes -= 1
+            while (byte & mask1):
+                num_bytes += 1
+                mask1 >>= 1
 
-    # If all characters were valid, num_bytes should be 0 at the end
+            # The first byte of a UTF-8 character can only be 2 to 4 bytes long
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            # Check that the byte starts with '10'
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        # Decrement the number of bytes remaining in the current UTF-8 character
+        num_bytes -= 1
+
+    # If there are leftover bytes, return False
     return num_bytes == 0
