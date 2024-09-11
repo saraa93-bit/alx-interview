3e760dadc24de4d14dd3bb40f8043d9289767045
saraa93-bit <saraa93177228@gmail.com>
2024-09-12 02:47:15 +0300

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
-"""
-Module to validate UTF-8 encoding
-"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
+    """method determines if given data set represents a valid UTF-8 encoding"""
     num_bytes = 0
-
-    # Masks to identify the leading bits of a byte
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
-    # Loop through each integer in the data list
     for num in data:
-        # Mask to get only the 8 least significant bits
-        byte = num & 0xFF
-
+        binary_rep = format(num, '#010b')[-8:]
         if num_bytes == 0:
-            # Determine how many bytes
-            if (byte & mask1) == 0:
-                # 1-byte character (ASCII)
-                continue
-            elif (byte & (mask1 >> 1)) == 0:
-                return False
-            while (byte & mask1):
+            for bit in binary_rep:
+                if bit == '0':
+                    break
                 num_bytes += 1
-                mask1 >>= 1
-
-            # The first byte of a UTF-8 character
+            if num_bytes == 0:
+                continue
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check that the byte starts with '10'
-            if not (byte & mask1 and not (byte & mask2)):
+            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
                 return False
-
-        # Decrement the number of bytes remaining
         num_bytes -= 1
-
-    # If there are leftover bytes, return False
     return num_bytes == 0
